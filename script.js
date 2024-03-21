cc786d00e5b143951d930d280b943b29e784a5a4
JimmyMP <81942440+JimmyMP@users.noreply.github.com>
2024-03-20 21:59:55 -0500

    Update script.js

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ setTimeout(cambiarSVG, 4500);
 
 setTimeout(function alerta1(){
     var nombre = "Jimmy";
-    alert(nombre + " te dice: Una plantita para otra plantita :3");
+    alert(nombre + " te dice: Para que te recuperes :3");
 }, 5300);
 
 // Función para cambiar el SVG después del proceso
