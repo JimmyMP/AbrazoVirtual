68d07f8ccd775b40ab2b87e272c38f4975ff1ef8
JimmyMP <81942440+JimmyMP@users.noreply.github.com>
2024-08-27 22:46:37 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,19 +2,19 @@ setTimeout(cambiaMensaje,1500)
 function cambiaMensaje(){
     document.getElementById("loading_message").innerText = "Un poco más..."
 }
-setTimeout(cambiaMensaje2,2500)
+setTimeout(cambiaMensaje2,3000)
 function cambiaMensaje2(){
     document.getElementById("loading_message").innerText = "Esta cariñoso..."
 }
-setTimeout(cambiaMensaje3,3500)
+setTimeout(cambiaMensaje3,4500)
 function cambiaMensaje3(){
     document.getElementById("loading_message").innerText = "Un poquitititito más..."
 }
-setTimeout(cambiaMensaje4,4500)
+setTimeout(cambiaMensaje4,6000)
 function cambiaMensaje4(){
     document.getElementById("loading_message").innerText = "Tulipanes Enviados"
 }
-setTimeout(cambiarSVG, 4500);
+setTimeout(cambiarSVG, 7500);
 
 
 setTimeout(function alerta1(){
